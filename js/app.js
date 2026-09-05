3ad5c0a845cb81e96b95fa286bfa17268727b8cb
Syzafid <syafrizalaf93@gmail.com>
2026-09-06 01:19:45 +0700

    up10

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  console.log('Initializing AR Melayu Multi-Marker Application...');
-
   UIManager.init();
   Gamification.init();
 
@@ -27,11 +25,32 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
   }
 
+  function sendToFlutter(msg) {
+    if (window.FlutterApp && typeof window.FlutterApp.postMessage === 'function') {
+      window.FlutterApp.postMessage(msg);
+    }
+  }
+
+  sendToFlutter('SET_PORTRAIT');
+
   const splash = document.getElementById('splash');
   const startBtn = document.getElementById('btn-start-ar');
 
   if (startBtn && splash) {
     startBtn.addEventListener('click', () => {
+      sendToFlutter('SET_LANDSCAPE');
+
+      if (screen.orientation && screen.orientation.lock) {
+        screen.orientation.lock('landscape').catch(() => {});
+      }
+
+      const videos = document.querySelectorAll('video');
+      videos.forEach(v => {
+        v.setAttribute('playsinline', '');
+        v.setAttribute('webkit-playsinline', '');
+        v.play().catch(() => {});
+      });
+
       UIManager.toggleFullscreen();
 
       splash.classList.add('hide');
